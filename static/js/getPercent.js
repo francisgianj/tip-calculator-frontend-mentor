73520b5911af226eda2583a094804b0560beb2fd
Francis <francisgian_fgm100@outlook.com>
2021-09-16 17:07:14 +0800

    deleted getPercent.js and did a lot of things

--- static/js/getPercent.js
+++
@@ -1,5 +0,0 @@
-const getPercent = (element) => {
-    var buttonValue = element.value;
-
-    document.querySelector("#testing").innerHTML = buttonValue;
-};
--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,54 @@
 
-document.addEventListener("DOMContentLoaded", ()=> {
-    document.addEventListener("keyup", (event)=> {
 
-        let billInput = document.querySelector("#bill");
+document.addEventListener("keyup", (event)=> {
 
-        let testing = document.querySelector("#testing");
+    let billInput = parseFloat(document.querySelector("#bill").value);
+    let numOfPeople = parseFloat(document.querySelector("#num-of-people").value);
 
-        testing.innerHTML = `${billInput.value}`;
-        return;
+    const tipValueContainer = parseFloat(document.querySelector("#tip-value-container").innerHTML);
 
-    });
+    const tipAmount = document.querySelector("#tip-amount");
+    const totalAmount = document.querySelector("#total-amount");
 
+    if (tipValueContainer != 0 && billInput) {
+        if (!numOfPeople) numOfPeople = 1;
 
+        const calculateTip = billInput * tipValueContainer;
+        tipAmount.innerHTML = `$${(calculateTip / numOfPeople).toFixed(2)}`;
+
+        const calculateTotal = (billInput + calculateTip) / numOfPeople;
+        totalAmount.innerHTML = `$${calculateTotal.toFixed(2)}`;
+    }
+
+    if (!billInput){
+        tipAmount.innerHTML = "$0.00";
+        totalAmount.innerHTML = "$0.00";
+    }
 
 });
+
+
+const getPercent = (element) => {
+    let buttonValue = parseFloat(element.value);
+    document.querySelector("#tip-value-container").innerHTML = buttonValue;
+
+    let billInput = parseFloat(document.querySelector("#bill").value);
+    let numOfPeople = parseFloat(document.querySelector("#num-of-people").value);
+    if (!numOfPeople) numOfPeople = 1;
+
+    if (billInput) {
+        const tipAmount = document.querySelector("#tip-amount");
+        const totalAmount = document.querySelector("#total-amount");
+
+        const calculateTip = billInput *  buttonValue;
+        tipAmount.innerHTML = `$${(calculateTip / numOfPeople).toFixed(2)}`;
+
+        const calculateTotal = (billInput + calculateTip) / numOfPeople;
+        totalAmount.innerHTML = `$${calculateTotal.toFixed(2)}`;
+        return;
+    }
+
+};
+
+
+
